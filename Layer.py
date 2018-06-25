20cce326b5a5eb288924698ce94f3dae00dcd9de
mdelsole <mdelsole64@gmail.com>
2018-06-25 11:20:40 -0400

    Initial commit

---
+++ Layer.py
@@ -0,0 +1,11 @@
+import Node
+
+class Layer(object):
+    """ 2D square array of Node objects.
+    :param length: number of nodes along each side of the square layer
+    """
+
+    def __init__(self, length, name):
+        self.length = length
+        self.nodes = [[Node.Node() for i in range(0, length)] for j in range(0, length)]
+        self.name = name
